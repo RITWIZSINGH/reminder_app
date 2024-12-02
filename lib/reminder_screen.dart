c3dc37239fdbecb1617c59efb4f1e3cabb1eab58
Ritwiz <madangopalsingh2803@gmail.com>
2024-12-02 22:06:26 +0530

    UI changes

--- lib/reminder_screen.dart
+++ lib/reminder_screen.dart
@@ -51,72 +51,113 @@ class _ReminderScreenState extends State<ReminderScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF0F4F8),
-      appBar: AppBar(
-        title: Text(
-          'My Reminders',
-          style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontFamily: "NexaBold"),
+      // Gradient background for the entire app
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFF0F4F8), Color(0xFFE0E7EF)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
         ),
-        backgroundColor: Color(0xFF4A6CF7),
-        elevation: 0,
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-        padding: EdgeInsets.all(16),
-        itemCount: _reminders.length,
-        itemBuilder: (context, index) {
-          final reminder = _reminders[index];
-          return Dismissible(
-            key: Key(reminder.id),
-            background: Container(
-              color: Colors.red,
-              alignment: Alignment.centerRight,
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Icon(Icons.delete, color: Colors.white),
-            ),
-            direction: DismissDirection.endToStart,
-            onDismissed: (_) => _deleteReminder(reminder.id),
-            child: AnimatedOpacity(
-              duration: Duration(milliseconds: 300),
-              opacity: reminder.isCompleted ? 0.5 : 1.0,
-              child: Card(
-                elevation: 4,
-                margin: EdgeInsets.symmetric(vertical: 8),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
+        child: Column(
+          children: [
+            // Modern AppBar with gradient
+            Container(
+              height: 120,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFF4A6CF7), Color.fromARGB(255, 21, 53, 234)],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
-                child: ListTile(
-                  title: Text(
-                    reminder.text,
-                    style: TextStyle(
-                      fontFamily: "NexaRegular",
-                      decoration: reminder.isCompleted
-                          ? TextDecoration.lineThrough
-                          : TextDecoration.none,
-                      color:
-                          reminder.isCompleted ? Colors.grey : Colors.black87,
-                    ),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black45,
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
                   ),
-                  trailing: Checkbox(
-                    activeColor: Color(0xFF4A6CF7),
-                    value: reminder.isCompleted,
-                    onChanged: (_) => _toggleReminderStatus(reminder.id),
+                ],
+              ),
+              child: SafeArea(
+                child: Center(
+                  child: Text(
+                    'My Reminders',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "NexaBold",
+                    ),
                   ),
                 ),
               ),
             ),
-          );
-        },
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView.builder(
+                  itemCount: _reminders.length,
+                  itemBuilder: (context, index) {
+                    final reminder = _reminders[index];
+                    return Dismissible(
+                      key: Key(reminder.id),
+                      background: Container(
+                        color: Colors.red.withAlpha(175),
+                        alignment: Alignment.centerRight,
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Icon(Icons.delete, color: Colors.white),
+                      ),
+                      direction: DismissDirection.endToStart,
+                      onDismissed: (_) => _deleteReminder(reminder.id),
+                      child: Card(
+                        elevation: 6,
+                        margin: EdgeInsets.symmetric(vertical: 8),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: ListTile(
+                          title: Text(
+                            reminder.text,
+                            style: TextStyle(
+                              fontFamily: "NexaRegular",
+                              fontSize: 16,
+                              decoration: reminder.isCompleted
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none,
+                              color: reminder.isCompleted
+                                  ? Colors.grey
+                                  : Colors.black87,
+                            ),
+                          ),
+                          trailing: Checkbox(
+                            activeColor: Color(0xFF4A6CF7),
+                            value: reminder.isCompleted,
+                            onChanged: (_) =>
+                                _toggleReminderStatus(reminder.id),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(0xFF4A6CF7),
-        onPressed: () {
+      // Gradient Floating Action Button
+      floatingActionButton: GestureDetector(
+        onTap: () {
           showModalBottomSheet(
             context: context,
             isScrollControlled: true,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            ),
             builder: (context) => Padding(
               padding: EdgeInsets.only(
                 bottom: MediaQuery.of(context).viewInsets.bottom,
@@ -133,15 +174,20 @@ class _ReminderScreenState extends State<ReminderScreen> {
                       hintText: 'Enter new reminder',
                       hintStyle: TextStyle(fontFamily: "NexaRegular"),
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+                        borderRadius: BorderRadius.circular(12),
                       ),
+                      filled: true,
+                      fillColor: Colors.white,
                     ),
                   ),
-                  SizedBox(height: 16),
+                  SizedBox(height: 14),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Color(0xFF4A6CF7),
-                      minimumSize: Size(double.infinity, 50),
+                      // padding: EdgeInsets.symmetric(vertical: 14),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
                     ),
                     onPressed: () {
                       _addReminder();
@@ -150,18 +196,38 @@ class _ReminderScreenState extends State<ReminderScreen> {
                     child: Text(
                       'Add Reminder'.toUpperCase(),
                       style: TextStyle(
-                          color: Colors.white, fontFamily: "NexaBold"),
+                          color: Colors.white,
+                          fontFamily: "NexaBold",
+                          fontSize: 16),
                     ),
                   ),
-                  SizedBox(height: 16),
+                  SizedBox(
+                    height: 14,
+                  )
                 ],
               ),
             ),
           );
         },
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
+        child: Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF4A6CF7), Color.fromARGB(255, 41, 67, 215)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Icon(Icons.add, color: Colors.white, size: 28),
         ),
       ),
     );
